6140ec64a1c51d52b6ea2afa2b0a64c9595be973
Antoine RICHARD <antoine_510@hotmail.fr>
2021-12-31 17:37:22 +0100

    Reworked library to decrease PAC1710 access count

--- PAC1710.cpp
+++ PAC1710.cpp
@@ -2,38 +2,50 @@
 #include <Wire.h>
 
 void PAC1710::Init(SenseScale ss, float senseMilliOhm) {
-	uint8_t ssval = ss;
-	writeByte(0x0a, 0x0e); // Vsource configuration
-	writeByte(0x0b, 0x78 + ssval); // Vsense configuration
-	fsc = (float)(10 * (1u << ssval)) / senseMilliOhm;
+	_ss = ss;
+	_fsc = (float)(10 * (1u << _ss)) / senseMilliOhm;
+	writeConfigRegisters();
 }
 
-float PAC1710::GetPower() {
-	// FSV * 1/65535 factored in as 0.00061
-	return GetPowerRatio() * 0.00061f * fsc;
+void PAC1710::SetSamplingTimesMs(unsigned voltage, unsigned current) {
+	if(voltage < 5) _samplingU = 0x0u;
+	else {
+		voltage /= 10;
+		_samplingU = 0x1u;
+		while(voltage > 0) {
+			voltage >>= 1u;
+			++_samplingU;
+		}
+	}
+	_samplingU &= 0x3u;
+	if(current < 5) _samplingI = 0x0u;
+	else {
+		current /= 10;
+		_samplingI = 0x1u;
+		while(current > 0) {
+			current >>= 1u;
+			++_samplingI;
+		}
+	}
+	_samplingI &= 0x7u;
+	writeConfigRegisters();
 }
 
-uint16_t PAC1710::GetPowerRatio() {
-	// Power ratio is in registers 0x15 << 8 + 0x16
-	return (readByte(0x15) << 8u) | readByte(0x16);
+void PAC1710::SetAveraging(Averaging voltage, Averaging current) {
+	_avgU = voltage;
+	_avgI = current;
+	writeConfigRegisters();
 }
 
-float PAC1710::GetCurrent() {
-	// 1/2047 factored in as 0.00048852
-	return GetCurrentRatio() * fsc * 0.00048852f;
+void PAC1710::ReadOnce(ReadSchedule schedule) {
+	if(schedule & READ_POWER) _powerRatio = (readByte(0x15) << 8u) | readByte(0x16);
+	if(schedule & READ_CURRENT) _currentRatio = (int16_t)((readByte(0x0d) << 8u) | readByte(0x0e)) >> 4;
+	if(schedule & READ_VOLTAGE) _voltageRatio = (readByte(0x11) << 3u) | (readByte(0x12) >> 5u);
 }
 
-int16_t PAC1710::GetCurrentRatio() {
-	return (int16_t)((readByte(0x0d) << 8u) | readByte(0x0e)) >> 4;
-}
-
-float PAC1710::GetVoltage() {
-	// FSV * 1/2047 factored in as 0.01953125
-	return GetVoltageRatio() * 0.01953125f;
-}
-
-uint16_t PAC1710::GetVoltageRatio() {
-	return (readByte(0x11) << 3u) | (readByte(0x12) >> 5u);
+void PAC1710::writeConfigRegisters() {
+	writeByte(0x0a, (_samplingU << 2) | _avgU);
+	writeByte(0x0b, (_samplingI << 4) | (_avgI << 2) | _ss);
 }
 
 uint8_t PAC1710::readByte(uint8_t address) {
--- PAC1710.h
+++ PAC1710.h
@@ -9,6 +9,16 @@
 class PAC1710 {
   public:
     /**
+     * @brief This is used to read a subset of all values available on the PAC1710
+     */
+    enum ReadSchedule {
+      READ_VOLTAGE  = 0x1u,
+      READ_CURRENT  = 0x2u,
+      READ_POWER    = 0x4u,
+      READ_ALL      = 0x7u
+    };
+
+    /**
      * @brief Maximum sense voltage. Chosen depending on sense resistor value
      * and maximum measured current.
      */
@@ -17,6 +27,16 @@ class PAC1710 {
       SS_20MV = 1,
       SS_40MV = 2,
       SS_80MV = 3
+    };
+
+    /**
+     * @brief Averaging of samples in hardware.
+     */
+    enum Averaging : byte {
+      AVG_NONE  = 0,
+      AVG_2     = 1,
+      AVG_4     = 2,
+      AVG_8     = 3
     };
 
     /**
@@ -30,37 +50,67 @@ class PAC1710 {
     void Init(SenseScale ss, float senseMilliOhm);
 
     /**
+     * @brief Sets the sampling time for voltage and current
+     * The sampling time will be rounded down to an available value:
+     * 2.5 x 2^n milliseconds
+     * Max is 20ms for voltage and 320ms for current
+     * @param voltage The voltage sampling time
+     * @param current The current sampling time
+     */
+    void SetSamplingTimesMs(unsigned voltage, unsigned current);
+
+    /**
+     * @brief Sets the number of samples to average for voltage and current
+     * @param voltage The voltage averaging sample count
+     * @param current The current averaging sample count
+     */
+    void SetAveraging(Averaging voltage, Averaging current);
+
+    /**
+     * @brief Triggers a read on the PAC1710 hardware
+     * This call is necessary before accessing the power, current or voltage
+     */
+    void ReadOnce(ReadSchedule schedule = READ_ALL);
+
+    /**
      * @brief Get measured power in watts
+     * FSV * 1/65535 factored in as 0.00061
      * 
      * @return float Power in watts
      */
-    float GetPower();
+    float GetPower() { return _powerRatio * 0.00061f * _fsc; }
 
     /**
      * @brief Get measured current in amps
+     * 1/2047 factored in as 0.00048852
      * 
      * @return float Current in amps
      */
-    float GetCurrent();
+    float GetCurrent() { return _currentRatio * _fsc * 0.00048852f; }
 
     /**
      * @brief Get measured voltage in volts
+     * FSV * 1/2047 factored in as 0.01953125
      * 
      * @return float Voltage in volts
      */
-    float GetVoltage();
+    float GetVoltage() { return _voltageRatio * 0.01953125f; }
     
   private:
     static constexpr uint8_t i2cAddr = 0x4c;
 
-    uint16_t GetPowerRatio();
-    int16_t GetCurrentRatio();
-    uint16_t GetVoltageRatio();
+    void writeConfigRegisters();
 
     uint8_t readByte(uint8_t address);
     void writeByte(uint8_t address, uint8_t value);
 
-    float fsc;
+    float _fsc;
+    SenseScale _ss = SS_80MV;  // 80mV sense scale, as per datasheet
+    byte _samplingU = 0x2u, _samplingI = 0x5u;  // 10ms voltage, 80ms current, as per datasheet.
+    Averaging _avgU = AVG_NONE, _avgI = AVG_NONE; // No averaging, as per datasheet.
+
+    int16_t _currentRatio;
+    uint16_t _voltageRatio, _powerRatio;
 };
 
 #endif
